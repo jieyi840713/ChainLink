21ed6876cb42095c595cc48b017e6a09623f1082
JayLee <jieyi840713@gmail.com>
2023-09-20 12:34:31 +0800

    Define our Chainlink VRF variables

--- src/ZombieFactory.sol
+++ src/ZombieFactory.sol
@@ -4,6 +4,9 @@ import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol";
 contract ZombieFactory is VRFConsumerbase {
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
+    bytes32 public keyHash;
+    uint256 public fee;
+    uint256 public randomResult;
 
     struct Zombie {
         string name;
@@ -18,7 +21,10 @@ contract ZombieFactory is VRFConsumerbase {
             0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625, // VRF Coordinator
             0x779877A7B0D9E8603169DdbD7836e478b4624789 // LINK Token
         )
-    {}
+    {
+        keyHash = 0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c;
+        fee = 100000000000000000;
+    }
 
     function _createZombie(string memory _name, uint _dna) private {
         zombies.push(Zombie(_name, _dna));
